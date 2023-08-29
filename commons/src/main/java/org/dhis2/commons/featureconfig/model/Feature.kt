90e677d7fbb4d43b6e3feb5752868e2c4e2df278
Pablo <ppajueloc@gmail.com>
2023-08-29 08:22:02 +0200

    enable/disable collapsible sections in forms

--- commons/src/main/java/org/dhis2/commons/featureconfig/model/Feature.kt
+++ commons/src/main/java/org/dhis2/commons/featureconfig/model/Feature.kt
@@ -1,3 +1,6 @@
 package org.dhis2.commons.featureconfig.model
 
-enum class Feature(val description: String)
+enum class Feature(val description: String) {
+    COMPOSE_FORMS("Use compose in forms"),
+    DISABLE_COLLAPSIBLE_SECTIONS("Disable collapsible sections")
+}
