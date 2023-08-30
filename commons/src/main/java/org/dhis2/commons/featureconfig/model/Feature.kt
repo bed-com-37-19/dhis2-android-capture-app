03dc38331fbb263a0b4bef4b7cd7991b095f33b0
Pablo <ppajueloc@gmail.com>
2023-08-30 11:46:51 +0200

    ktlint

--- commons/src/main/java/org/dhis2/commons/featureconfig/model/Feature.kt
+++ commons/src/main/java/org/dhis2/commons/featureconfig/model/Feature.kt
@@ -2,5 +2,5 @@ package org.dhis2.commons.featureconfig.model
 
 enum class Feature(val description: String) {
     COMPOSE_FORMS("Use compose in forms"),
-    DISABLE_COLLAPSIBLE_SECTIONS("Disable collapsible sections")
+    DISABLE_COLLAPSIBLE_SECTIONS("Disable collapsible sections"),
 }
